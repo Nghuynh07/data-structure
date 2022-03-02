3e38a1e6d333799aac4e8c8d47f68acf9290e778
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-03-02 09:17:01 -0800

    push pop

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -12,7 +12,41 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      let removingTail = this.tail;
+      this.tail.next = newNode;
+      this.tail = this.tail.next;
+      this.tail.prev = removingTail;
+
+      //   this.tail.next = newNode;
+      //   newNode.prev = this.tail;
+      //   this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    let removingTail = this.tail;
+    if (this.length === 0) return undefined;
+    else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      removingTail.prev = null;
+    }
+    this.length--;
+    return removingTail;
+  }
 }
 
-let first = new Node(11);
-console.log(first);
+let list = new DoublyLinkedList();
+list.push(20);
+list.push(30);
+list.push("last item");
+list.pop();
+console.log(list);
