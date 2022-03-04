aab04bc71ede4749cc3f31a35aa54b4791b3cc6d
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-03-04 08:23:34 -0800

    binarySearchTrees

---
+++ binarySearchTrees.js
@@ -0,0 +1,50 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value === current.value) return undefined;
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          }
+          current = current.left;
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          }
+          current = current.left;
+        }
+      }
+    }
+  }
+}
+
+let tree = new BinarySearchTree();
+tree.insert(100);
+tree.insert(40);
+tree.insert(102);
+tree.insert(99);
+tree.insert(11);
+tree.insert(45);
+// tree.insert(45);
+
+console.log(tree);
